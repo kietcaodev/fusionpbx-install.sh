d85539232080ccc531f3991f615372f5bbf22ad4
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2025-12-18 09:45:38 +0700

    Clone fusionpbx-install.sh repository in pre-install

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -6,5 +6,8 @@ apt-get update && apt-get upgrade -y
 #install packages
 apt-get install -y git lsb-release
 
+#clone source
+git clone https://github.com/kietcaodev/fusionpbx-install.sh.git
+
 #change the working directory
 cd /usr/src/fusionpbx-install.sh/debian
